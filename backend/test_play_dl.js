ef11f28bca07ff92ff0a3c7cfb885cee6329bdc9
DJ-2007 <jethva.dhaval.1155@gmail.com>
2026-07-29 21:36:50 +0530

    Remove test and debug files

--- backend/test_play_dl.js
+++
@@ -1,7 +0,0 @@
-import play from 'play-dl';
-async function test() {
-  const streamInfo = await play.stream('https://www.youtube.com/watch?v=dQw4w9WgXcQ', { discordPlayerCompatibility: true });
-  console.log(Object.keys(streamInfo));
-  console.log('url:', streamInfo.url);
-}
-test().catch(console.error);
--- backend/test_ytdl.js
+++
@@ -1,7 +0,0 @@
-import ytdl from '@distube/ytdl-core';
-async function test() {
-  const info = await ytdl.getInfo('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-  const format = ytdl.chooseFormat(info.formats, { filter: 'audioonly' });
-  console.log(format.url);
-}
-test().catch(console.error);
