9779bca9aa37796ebe6d661b697badb63c7c17d5
DJ-2007 <jethva.dhaval.1155@gmail.com>
2026-08-04 16:38:00 +0530

    Add expanded YouTube Music playlists across genres

--- frontend/src/data/popularPlaylists.js
+++ frontend/src/data/popularPlaylists.js
@@ -1,9 +1,9 @@
-// Curated Popular Spotify Playlists & Featured Content
+// Curated Best YouTube Music Playlists & Trending Hits
 
 export const POPULAR_PLAYLISTS = [
   {
     name: 'Hit Songs English',
-    subtitle: 'Curated • Top Global Hits',
+    subtitle: 'YouTube Music • Top Global Hits',
     cover: 'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500',
     type: 'Playlist',
     isPinned: true,
@@ -12,11 +12,12 @@ export const POPULAR_PLAYLISTS = [
       { id: 'eng-2', videoId: '34Na4j8AVgA', title: 'Starboy', artist: 'The Weeknd ft. Daft Punk', duration: 230, thumbnail: 'https://img.youtube.com/vi/34Na4j8AVgA/hqdefault.jpg' },
       { id: 'eng-3', videoId: 'H5v3kku4y6Q', title: 'As It Was', artist: 'Harry Styles', duration: 167, thumbnail: 'https://img.youtube.com/vi/H5v3kku4y6Q/hqdefault.jpg' },
       { id: 'eng-4', videoId: 'TUVcZfQe-Kw', title: 'Levitating', artist: 'Dua Lipa', duration: 203, thumbnail: 'https://img.youtube.com/vi/TUVcZfQe-Kw/hqdefault.jpg' },
+      { id: 'eng-5', videoId: '09R8_2nJtjg', title: 'Sugar', artist: 'Maroon 5', duration: 235, thumbnail: 'https://img.youtube.com/vi/09R8_2nJtjg/hqdefault.jpg' },
     ]
   },
   {
     name: 'Same Beef & Punjabi Hits',
-    subtitle: 'Curated • Bohemia, Sidhu Moose Wala',
+    subtitle: 'YouTube Music • Bohemia, Sidhu Moose Wala',
     cover: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=500',
     type: 'Playlist',
     isPinned: true,
@@ -25,27 +26,42 @@ export const POPULAR_PLAYLISTS = [
       { id: 'pun-2', videoId: 'cWMxCE248B0', title: 'Softly', artist: 'Karan Aujla', duration: 154, thumbnail: 'https://img.youtube.com/vi/cWMxCE248B0/hqdefault.jpg' },
       { id: 'pun-3', videoId: 'vX2cDW8LUWk', title: 'Excuses', artist: 'AP Dhillon & Gurinder Gill', duration: 176, thumbnail: 'https://img.youtube.com/vi/vX2cDW8LUWk/hqdefault.jpg' },
       { id: 'pun-4', videoId: 'n_FCrCQ6UT5', title: '295', artist: 'Sidhu Moose Wala', duration: 270, thumbnail: 'https://img.youtube.com/vi/n_FCrCQ6UT5/hqdefault.jpg' },
+      { id: 'pun-5', videoId: 'cl0a3iBNyMg', title: 'Cheques', artist: 'Shubh', duration: 183, thumbnail: 'https://img.youtube.com/vi/cl0a3iBNyMg/hqdefault.jpg' },
+    ]
+  },
+  {
+    name: 'Bollywood Top 50',
+    subtitle: 'YouTube Music • Arijit Singh, Pritam',
+    cover: 'https://images.unsplash.com/photo-1518609878373-06d740f60d8b?w=500',
+    type: 'Playlist',
+    isPinned: true,
+    songs: [
+      { id: 'bol-1', videoId: 'V7LwfY5U_BU', title: 'Ranjha (From "Shershaah")', artist: 'Jasleen Royal, B Praak, Romy', duration: 228, thumbnail: 'https://img.youtube.com/vi/V7LwfY5U_BU/hqdefault.jpg' },
+      { id: 'bol-2', videoId: 'BddP6PYo2gs', title: 'Kesariya', artist: 'Arijit Singh, Pritam', duration: 268, thumbnail: 'https://img.youtube.com/vi/BddP6PYo2gs/hqdefault.jpg' },
+      { id: 'bol-3', videoId: 'tLqtnGLb54E', title: 'Chaleya (From "Jawan")', artist: 'Arijit Singh, Shilpa Rao', duration: 200, thumbnail: 'https://img.youtube.com/vi/tLqtnGLb54E/hqdefault.jpg' },
+      { id: 'bol-4', videoId: 'vS_a8Ed46Lg', title: 'Tum Hi Ho', artist: 'Arijit Singh', duration: 262, thumbnail: 'https://img.youtube.com/vi/vS_a8Ed46Lg/hqdefault.jpg' },
     ]
   },
   {
     name: 'Mind Relaxing Music 2026',
-    subtitle: 'Playlist • Chill Vibes',
+    subtitle: 'YouTube Music • Ambient & Chill Vibes',
     cover: 'https://images.unsplash.com/photo-1518241353330-0f7941c2d9b5?w=500',
     type: 'Playlist',
     songs: [
       { id: 'chill-1', videoId: '2OEL4P1rub0', title: 'Mind Relaxing Rain & Flute', artist: 'Peaceful Chill', duration: 300, thumbnail: 'https://img.youtube.com/vi/2OEL4P1rub0/hqdefault.jpg' },
       { id: 'chill-2', videoId: 'jfKfPfyJRdk', title: 'Lofi Hip Hop Beats to Relax', artist: 'Lofi Girl', duration: 240, thumbnail: 'https://img.youtube.com/vi/jfKfPfyJRdk/hqdefault.jpg' },
+      { id: 'chill-3', videoId: 'UfcAVejslrU', title: 'Weightless', artist: 'Marconi Union', duration: 480, thumbnail: 'https://img.youtube.com/vi/UfcAVejslrU/hqdefault.jpg' },
     ]
   },
   {
     name: 'Morning peace 🕊️✌️',
-    subtitle: 'Playlist • Soft & Peaceful',
+    subtitle: 'YouTube Music • Soft & Peaceful',
     cover: 'https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=500',
     type: 'Playlist',
-    isPinned: false,
     songs: [
       { id: 'morn-1', videoId: 'V7LwfY5U_BU', title: 'Ranjha (From "Shershaah")', artist: 'Jasleen Royal, B Praak', duration: 228, thumbnail: 'https://img.youtube.com/vi/V7LwfY5U_BU/hqdefault.jpg' },
       { id: 'morn-2', videoId: 'BddP6PYo2gs', title: 'Kesariya', artist: 'Arijit Singh', duration: 268, thumbnail: 'https://img.youtube.com/vi/BddP6PYo2gs/hqdefault.jpg' },
+      { id: 'morn-3', videoId: 'gvyUuxdRdR4', title: 'Lag Ja Gale', artist: 'Lata Mangeshkar', duration: 255, thumbnail: 'https://img.youtube.com/vi/gvyUuxdRdR4/hqdefault.jpg' },
     ]
   },
   {
@@ -58,12 +74,24 @@ export const POPULAR_PLAYLISTS = [
     ]
   },
   {
-    name: 'Bharat Ke Aslee Superheroes',
-    subtitle: 'Podcast • Spotify Studios',
-    cover: 'https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?w=500',
-    type: 'Podcast',
+    name: 'Gym Workout & Hype Beats',
+    subtitle: 'YouTube Music • High Energy EDM & Beats',
+    cover: 'https://images.unsplash.com/photo-1517838277536-f5f99be501cd?w=500',
+    type: 'Playlist',
     songs: [
-      { id: 'hero-1', videoId: '3x4v5b6c7y8', title: 'Unsung Heroes of India', artist: 'Spotify Originals', duration: 420, thumbnail: 'https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?w=500' },
+      { id: 'gym-1', videoId: 'K4DyBUG242c', title: 'Numb / Encore', artist: 'Jay-Z & Linkin Park', duration: 205, thumbnail: 'https://img.youtube.com/vi/K4DyBUG242c/hqdefault.jpg' },
+      { id: 'gym-2', videoId: 'YqeW9_5kURI', title: 'Lean On', artist: 'Major Lazer & DJ Snake', duration: 176, thumbnail: 'https://img.youtube.com/vi/YqeW9_5kURI/hqdefault.jpg' },
+      { id: 'gym-3', videoId: 'OPf0YbXqDm0', title: 'Uptown Funk', artist: 'Mark Ronson ft. Bruno Mars', duration: 270, thumbnail: 'https://img.youtube.com/vi/OPf0YbXqDm0/hqdefault.jpg' },
+    ]
+  },
+  {
+    name: 'Retro 90s & 2000s Classics',
+    subtitle: 'YouTube Music • Evergreen Golden Hits',
+    cover: 'https://images.unsplash.com/photo-1468164016595-6108e4c60c8b?w=500',
+    type: 'Playlist',
+    songs: [
+      { id: 'retro-1', videoId: 'gvyUuxdRdR4', title: 'Lag Ja Gale Se Phir', artist: 'Lata Mangeshkar', duration: 255, thumbnail: 'https://img.youtube.com/vi/gvyUuxdRdR4/hqdefault.jpg' },
+      { id: 'retro-2', videoId: 'vS_a8Ed46Lg', title: 'Tere Bina Zindagi Se', artist: 'Kishore Kumar & Lata Mangeshkar', duration: 330, thumbnail: 'https://img.youtube.com/vi/vS_a8Ed46Lg/hqdefault.jpg' },
     ]
   }
 ];
